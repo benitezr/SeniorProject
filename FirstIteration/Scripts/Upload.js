8c050e433d2b7c764b3bfb9705080fd3789e3ed9
Rich <n00783340@ospreys.unf.edu>
2017-04-06 05:10:50 -0400

    Modal not discarded if is in use

--- FirstIteration/Scripts/Upload.js
+++ FirstIteration/Scripts/Upload.js
@@ -1,10 +1,17 @@
-﻿$("#upload-list li").click(function () {
+﻿var modalID;
+$("#upload-list li").click(function () {
     var id = $(this).attr("id");
-    $.ajax({
-        url: "/Chart/UploadModal/" + id,
-        success: function (data) {
-            $("#modal-wrapper").html(data);
-            $("#upload-modal").modal();
-        }
-    });
+    if(!modalID || modalID !== id){
+        $.ajax({
+            url: "/Chart/UploadModal/" + id,
+            success: function (data) {
+                $("#modal-wrapper").html(data);
+                $("#upload-modal").modal();
+                //$(document).find("#submit").attr("onclick", "");
+                modalID = id;
+            }
+        });   
+    } else {
+        $("#upload-modal").modal("toggle");
+    }
 });
