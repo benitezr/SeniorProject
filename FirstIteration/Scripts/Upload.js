0385a84ce78fcdc2328a4d68fd0a8fa1c9a49d1c
Rich <n00783340@ospreys.unf.edu>
2017-04-08 15:42:55 -0400

    Added bootstrap filestyle

--- FirstIteration/Scripts/Upload.js
+++ FirstIteration/Scripts/Upload.js
@@ -1,17 +1,31 @@
-﻿var modalID;
-$("#upload-list li").click(function () {
-    var id = $(this).attr("id");
-    if(!modalID || modalID !== id){
-        $.ajax({
-            url: "/Chart/UploadModal/" + id,
-            success: function (data) {
-                $("#modal-wrapper").html(data);
-                $("#upload-modal").modal();
-                //$(document).find("#submit").attr("onclick", "");
-                modalID = id;
-            }
-        });   
-    } else {
-        $("#upload-modal").modal("toggle");
-    }
+﻿$(function () {
+    var modalID;
+    $("#upload-list li").click(function () {
+        var id = $(this).attr("id");
+        if (!modalID || modalID !== id) {
+            $.ajax({
+                url: "/Chart/UploadModal/" + id,
+                success: function (data) {
+                    $("#modal-wrapper").html(data);
+                    modalID = id;
+                },
+                complete: function () {
+                    $("#submit").on("click", function () {
+                        alert("submit button clicked");
+                    });
+
+                    $(":file").filestyle({
+                        buttonText: "Find CSV",
+                        icon: false,
+                        placeholder: "No file chosen",
+                        buttonName: "btn-primary"
+                    });
+
+                    $("#upload-modal").modal();
+                }
+            });
+        } else {
+            $("#upload-modal").modal("toggle");
+        }
+    });
 });
