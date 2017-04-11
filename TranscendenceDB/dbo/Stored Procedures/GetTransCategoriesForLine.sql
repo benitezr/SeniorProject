848e53756ddd6a3c6442aa38fab4e40d8c882f2a
Rich <n00783340@ospreys.unf.edu>
2017-04-10 22:10:58 -0400

    Testing on localdb

---
+++ TranscendenceDB/dbo/Stored Procedures/GetTransCategoriesForLine.sql
@@ -0,0 +1,15 @@
+﻿CREATE procedure [dbo].[GetTransCategoriesForLine]
+(
+	@deptid as int = null,
+	@fundcategory as varchar(5) = '%'
+)
+as
+begin
+	select datepart(month, TransDate) as [TransMonth], FundCategory, Sum(TransAmount) as [TransAmount]
+	from Funding_Sources, Transactions
+	where (@deptid is null or DeptID = @deptid) and
+	Transactions.FundMasterID = Funding_Sources.FundMasterID and
+	FundCategory like @fundcategory
+	group by FundCategory, datepart(year, TransDate), datepart(month, TransDate)
+	order by datepart(month, TransDate) asc
+end
---
+++ TranscendenceDB/dbo/Tables/Departments.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[Departments] (
+    [DeptID]   INT       NOT NULL,
+    [DeptName] CHAR (50) NOT NULL,
+    PRIMARY KEY CLUSTERED ([DeptID] ASC)
+);
+
