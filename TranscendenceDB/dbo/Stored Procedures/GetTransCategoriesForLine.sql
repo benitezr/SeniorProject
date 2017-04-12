1c7c863bb28fdd7517c5920fc6031cde5225f907
Rich <n00783340@ospreys.unf.edu>
2017-04-11 20:04:08 -0400

    Altered stored procedure

--- TranscendenceDB/dbo/Stored Procedures/GetTransCategoriesForLine.sql
+++ TranscendenceDB/dbo/Stored Procedures/GetTransCategoriesForLine.sql
@@ -5,11 +5,22 @@
 )
 as
 begin
-	select datepart(month, TransDate) as [TransMonth], FundCategory, Sum(TransAmount) as [TransAmount]
-	from Funding_Sources, Transactions
-	where (@deptid is null or DeptID = @deptid) and
-	Transactions.FundMasterID = Funding_Sources.FundMasterID and
-	FundCategory like @fundcategory
-	group by FundCategory, datepart(year, TransDate), datepart(month, TransDate)
-	order by datepart(month, TransDate) asc
+	select MonthList.Months as [Month], isnull(F.FundCategory, C.FundCategory) as [Category], isnull(F.Amount, 0) as [Amount]
+	from (
+		values (1), (2), (3), (4), (5), 
+				(6), (7), (8), (9), (10), (11), (12)
+	) [MonthList](Months)
+	Left Join (
+		select datepart(month, TransDate) as [Month],FundCategory, Sum(TransAmount) as [Amount]
+		from Transactions, Funding_Sources
+		where (@deptid is null or DeptID = @deptid) and Transactions.FundMasterID = Funding_Sources.FundMasterID
+		group by FundCategory, datepart(month, TransDate)
+	) F on F.[Month] = [Months]
+	Left Join (
+		select FundCategory
+		from Transactions, Funding_Sources
+		where (@deptid is null or DeptID = @deptid) and Transactions.FundMasterID = Funding_Sources.FundMasterID
+		group by FundCategory
+	) C on F.Amount is null and F.FundCategory is null
+	order by [Month]
 end
