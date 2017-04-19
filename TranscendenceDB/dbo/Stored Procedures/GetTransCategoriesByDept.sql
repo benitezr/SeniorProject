cb55d425d137c44707e92706f3d6211f122dee94
Rich <n00783340@ospreys.unf.edu>
2017-04-18 21:33:47 -0400

    Stored procedures updated to take Year param

--- TranscendenceDB/dbo/Stored Procedures/GetTransCategoriesByDept.sql
+++ TranscendenceDB/dbo/Stored Procedures/GetTransCategoriesByDept.sql
@@ -1,12 +1,13 @@
 ﻿CREATE procedure [dbo].[GetTransCategoriesByDept]
 (
+	@year int,
 	@deptid int = null,
 	@fundcategory varchar(10) = null,
 	@staffid int = null
 )
 as
 begin
-	select MonthList.Months as [Month], C.Category as [Category], isnull(F.Amount, 0) as [Amount]
+	select C.Category as [Category], isnull(F.Amount, 0) as [Amount]
 	from (
 		values (1), (2), (3), (4), (5), 
 				(6), (7), (8), (9), (10), (11), (12)
@@ -16,7 +17,7 @@ begin
 		from Transactions, Funding_Sources
 		where (@deptid is null or DeptID = @deptid) and Transactions.FundMasterID = Funding_Sources.FundMasterID and 
 			(@fundcategory is null or FundCategory like '%' + @fundcategory + '%') and
-			(@staffid is null or StaffID = @staffid)
+			(@staffid is null or StaffID = @staffid) and datepart(year, TransDate) = @year
 		group by case when @fundcategory is null then FundCategory else FundCodeName end
 	) C on C.Category is not null
 	Left Join (
@@ -25,7 +26,7 @@ begin
 		from Transactions, Funding_Sources
 		where (@deptid is null or DeptID = @deptid) and Transactions.FundMasterID = Funding_Sources.FundMasterID and 
 			(@fundcategory is null or FundCategory like '%' + @fundcategory + '%') and
-			(@staffid is null or StaffID = @staffid)
+			(@staffid is null or StaffID = @staffid) and datepart(year, TransDate) = @year
 		group by case when @fundcategory is null then FundCategory else FundCodeName end, datepart(month, TransDate)
 	) F on F.[Month] = [Months] and F.Category = C.Category 
 	order by Months
--- TranscendenceDB/dbo/Stored Procedures/GetTransCategoriesForPie.sql
+++ TranscendenceDB/dbo/Stored Procedures/GetTransCategoriesForPie.sql
@@ -1,5 +1,6 @@
 ﻿CREATE PROCEDURE [dbo].[GetTransCategoriesForPie]
 (
+	@year int,
 	@deptid int = null,
 	@fundcategory varchar(10) = null,
 	@staffid int = null
@@ -10,11 +11,11 @@ SET @amtSum = (select Sum(TransAmount) as [Amount]
 			from Funding_Sources F, Transactions T
 			where T.FundMasterID = F.FundMasterID and (@deptid is null or DeptID = @deptid) and 
 			(@fundcategory is null or FundCategory like '%' + @fundcategory + '%') and
-			(@staffid is null or StaffID = @staffid))
+			(@staffid is null or StaffID = @staffid) and datepart(year, TransDate) = @year)
 
 SELECT CASE WHEN @fundcategory is null THEN FundCategory ELSE FundCodeName END as [Category], (SUM(TransAmount) / @amtSum) * 100 as [Amount]
 from Funding_Sources F, Transactions T
 where T.FundMasterID = F.FundMasterID and (@deptid is null or DeptID = @deptid) and 
 			(@fundcategory is null or FundCategory like '%' + @fundcategory + '%') and
-			(@staffid is null or StaffID = @staffid)
+			(@staffid is null or StaffID = @staffid) and datepart(year, TransDate) = @year
 group by case when @fundcategory is null then FundCategory else FundCodeName end
