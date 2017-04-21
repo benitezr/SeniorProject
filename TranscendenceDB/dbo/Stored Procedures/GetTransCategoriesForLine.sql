9ce71a958705e2ab5523b231ed5a6d02e27ef7a5
Rich <n00783340@ospreys.unf.edu>
2017-04-20 23:23:34 -0400

    New dbproject

---
+++ TranscendenceDB/dbo/Stored Procedures/GetTransCategoriesForLine.sql
@@ -0,0 +1,26 @@
+﻿CREATE procedure [dbo].[GetTransCategoriesForLine]
+(
+	@deptid as int = null,
+	@fundcategory as varchar(5) = '%'
+)
+as
+begin
+	select MonthList.Months as [Month], isnull(F.FundCategory, C.FundCategory) as [Category], isnull(F.Amount, 0) as [Amount]
+	from (
+		values (1), (2), (3), (4), (5), 
+				(6), (7), (8), (9), (10), (11), (12)
+	) [MonthList](Months)
+	Left Join (
+		select datepart(month, TransDate) as [Month],FundCategory, Sum(TransAmount) as [Amount]
+		from Transactions, Funding_Sources
+		where (@deptid is null or DeptID = @deptid) and Transactions.FundMasterID = Funding_Sources.FundMasterID
+		group by FundCategory, datepart(month, TransDate)
+	) F on F.[Month] = [Months]
+	Left Join (
+		select FundCategory
+		from Transactions, Funding_Sources
+		where (@deptid is null or DeptID = @deptid) and Transactions.FundMasterID = Funding_Sources.FundMasterID
+		group by FundCategory
+	) C on F.Amount is null and F.FundCategory is null
+	order by [Month]
+end
---
+++ TranscendenceDB/dbo/Tables/Staff.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Staff] (
+    [StaffID]   INT          NOT NULL,
+    [DeptID]    INT          NOT NULL,
+    [StaffName] VARCHAR (35) NOT NULL,
+    PRIMARY KEY CLUSTERED ([StaffID] ASC),
+    CONSTRAINT [FK_DeptID] FOREIGN KEY ([DeptID]) REFERENCES [dbo].[Departments] ([DeptID]) ON DELETE CASCADE ON UPDATE CASCADE
+);
+
