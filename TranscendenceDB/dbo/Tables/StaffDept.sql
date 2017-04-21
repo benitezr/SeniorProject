f04e99a9f0573cc28eec9bc3c8a5aedaaa4ff05e
Rich <n00783340@ospreys.unf.edu>
2017-04-20 23:31:17 -0400

    Added Staff associative table

---
+++ TranscendenceDB/dbo/Tables/StaffDept.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[StaffDept]
+(
+	[StaffID] INT NOT NULL, 
+    [DeptID] INT NOT NULL,
+	CONSTRAINT PK_StaffID_DeptID PRIMARY KEY (StaffID, DeptID),
+	CONSTRAINT [FK_StaffID_Bridge] FOREIGN KEY ([StaffID]) REFERENCES [dbo].[Staff] ([StaffID]),
+	CONSTRAINT [FK_DeptID_Bridge] FOREIGN KEY ([DeptID]) REFERENCES [dbo].[Departments] ([DeptID]) 
+)
